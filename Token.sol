a05a2796cabf43d651bb64ac32d9590cacd5a310
Moibe <moi_estrello@hotmail.com>
2021-12-23 12:47:35 -0600

    Initial commit

---
+++ Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+import "https://github.com/Moibe/Guppy/yields-utils-v2/token/ERC20.sol";
+
+pragma solidity >=0.4.0 <0.9.0;
+
+contract Bitcoin is ERC20 {
+  constructor () public ERC20("Bitcoin", "BTC", 18) {
+    _mint(msg.sender, 21000000 * (10 ** uint256(decimals())));
+  }
+}
