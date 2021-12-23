dd77d02fc2a9292d6706bc4d4e88134ff2a7a83a
Moibe <moi_estrello@hotmail.com>
2021-12-23 12:52:30 -0600

    Tokens cifra completa.
    Se corrigió la línea que al parecer no acepta decimals para poner la palabra completa.

--- Token.sol
+++ Token.sol
@@ -6,6 +6,6 @@ pragma solidity >=0.4.0 <0.9.0;
 
 contract Bitcoin is ERC20 {
   constructor () public ERC20("Bitcoin", "BTC", 18) {
-    _mint(msg.sender, 21000000 * (10 ** uint256(decimals())));
+    _mint(msg.sender, 21000000000000000000000000); //equivalente a 21 millones de tokens.
   }
 }
